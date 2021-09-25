868ea7e9f5f55ffe18ccce20a48de10245b7cead
toddmac12 <toddmac12@gmail.com>
2021-09-25 18:24:34 -0400

    week 9 homework

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+     [Route("/")]
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Ayden";
+      return View(myLetterVariable);
+
+  }
+}}
